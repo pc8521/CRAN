ae0192fb373ce827175a5af9135d328cb6ea3817
raymond <pc8521@gmail.com>
2025-10-12 14:15:56 +0800

    create postgres database

--- config/settings.py
+++ config/settings.py
@@ -46,12 +46,13 @@ DJANGO_APPS = [
 ]
 
 APPLICATION_APPS = [
-    'pages.apps.PagesConfig', 
+    'pages.apps.PagesConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,8 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cran',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
