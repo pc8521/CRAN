bcf295c244fef5aaf842872c268643f6d250cb01
raymond <pc8521@gmail.com>
2025-10-23 12:27:48 +0800

    202510231227

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse 
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from products.models import Category, Product
 from products.choices import brand_choices, tag_choices
-# Create your views here. 
 
 def register(request): 
     if request.method == 'POST':
@@ -14,30 +15,37 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-        if password == password2:
-            if User.objects.filter(username=username).exists():
-                messages.error(request, 'Username already exists')
-                return redirect('accounts:register')
-            else:
-                if User.objects.filter(email=email).exists():
-                    messages.error(request, 'Email already exists')
-                    return redirect('accounts:register')
-                else:
-                    user=User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-                    user.save()
-                    messages.success(request, 'You are now registered and can log in')
-                    return redirect('accounts:login')
-        else:
+        
+        if password != password2:
             messages.error(request, 'Passwords do not match')
-            return redirect('accounts:register')
+            return render(request, 'accounts/register.html', {'first_name': first_name, 'last_name': last_name, 'username': username, 'email': email})
+            
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists')
+            return render(request, 'accounts/register.html', {'first_name': first_name, 'last_name': last_name, 'username': username, 'email': email})
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists')
+            return render(request, 'accounts/register.html', {'first_name': first_name, 'last_name': last_name, 'username': username, 'email': email})
+
+        user = User.objects.create_user(
+            username=username, 
+            password=password, 
+            email=email, 
+            first_name=first_name, 
+            last_name=last_name
+        )
+        user.save()
+        messages.success(request, 'You are now registered and can log in')
+        return redirect('accounts:login')
     else:
         return render(request, 'accounts/register.html')
 
 def login(request): 
     if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user=auth.authenticate(username=username, password=password)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
@@ -48,18 +56,20 @@ def login(request):
     else:
         return render(request, 'accounts/login.html')
 
+@require_POST
 def logout(request): 
-    if request.method == 'POST':
-        auth.logout(request)
+    auth.logout(request)
+    messages.success(request, 'You are now logged out')
     return redirect('pages:index')
 
+@login_required
 def dashboard(request):
-    product=Product.objects.all()
-    categories=Category.objects.all()
-    context={
+    products = Product.objects.select_related('category').all()
+    categories = Category.objects.all()
+    context = {
         'categories': categories,
         'brand_choices': brand_choices,
         'tag_choices': tag_choices,
-        'product': product
-        }
+        'products': products,
+    }
     return render(request, 'accounts/dashboard.html', context)
